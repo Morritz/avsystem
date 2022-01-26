3b9c210f1d5b17835e027b2cf83e067866713ae4
Morritz <12800230+Morritz@users.noreply.github.com>
2022-01-26 11:20:45 +0100

    config parsing

--- src/modules/Config.ts
+++ src/modules/Config.ts
@@ -1,13 +1,43 @@
 import { FCFSScheduler, Scheduler } from "./Scheduler";
+import fs from "fs";
 
+/*
+schema
+{
+  scheduler: "FCFS",
+  elevators: 16,
+  maxFloor: 16
+}
+*/
 export class Config {
+  private parsedConfig: any;
+  constructor() {
+    this.parsedConfig = {};
+    try {
+      const jsonString = fs.readFileSync("./config.json");
+      this.parsedConfig = JSON.parse(jsonString.toString());
+    } catch (err) {
+      console.log(err);
+    }
+  }
   public loadScheduler(): Scheduler {
+    const str = this.parsedConfig.scheduler;
+    if (typeof str === "string") {
+      switch (str) {
+        case "FCFS":
+          return new FCFSScheduler();
+      }
+    }
     return new FCFSScheduler();
   }
   public getElevatorsCount(): number {
+    const num = this.parsedConfig.elevators;
+    if (typeof num === "number" && num > 0) return num;
     return 16;
   }
   public getMaxFloor(): number {
+    const num = this.parsedConfig.maxFloor;
+    if (typeof num === "number" && num > 0) return num;
     return 16;
   }
 }
